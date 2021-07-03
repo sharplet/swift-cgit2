f1c5e899dd8cbd3199ceffc4cae22447890ca284
Adam Sharp <adam@sharplet.me>
2021-07-03 16:51:02 -0400

    v1.1.1
    libgit2 release notes: https://github.com/libgit2/libgit2/releases/tag/v1.1.1.

--- Package.swift
+++ Package.swift
@@ -21,18 +21,18 @@ let package = Package(
 
     .binaryTarget(
       name: "OpenSSL",
-      url: "https://github.com/sharplet/swift-cgit2/releases/download/v1.1.0/OpenSSL.xcframework.zip",
-      checksum: "cf95859d15d8f61a229023e70ed3ece532e47a143247de56931c329a029c7d35"
+      url: "https://github.com/sharplet/swift-cgit2/releases/download/v1.1.1/OpenSSL.xcframework.zip",
+      checksum: "65afedb6e49bfc58dbb785ff771592c3f8b093e595a11c99b271ece92d400fda"
     ),
     .binaryTarget(
       name: "libgit2",
-      url: "https://github.com/sharplet/swift-cgit2/releases/download/v1.1.0/libgit2.xcframework.zip",
-      checksum: "6252d651a74ef953b29d7144ed20cf91be2ebe7f148ea3c0eaba00ddca1cd8cd"
+      url: "https://github.com/sharplet/swift-cgit2/releases/download/v1.1.1/libgit2.xcframework.zip",
+      checksum: "433fc5d4cb892325b4469eadf1eda33ede2663340cc36857484d166cc0ad5a99"
     ),
     .binaryTarget(
       name: "libssh2",
-      url: "https://github.com/sharplet/swift-cgit2/releases/download/v1.1.0/libssh2.xcframework.zip",
-      checksum: "7b34a75674b622cc98c7a524a555817abdea455220070bdfb91b9234f4217c8e"
+      url: "https://github.com/sharplet/swift-cgit2/releases/download/v1.1.1/libssh2.xcframework.zip",
+      checksum: "ceceaeaf84cc2e89a61016c6951ba5bd27a75e3d0cd8ae21e328ad158ace340d"
     ),
   ]
 )
